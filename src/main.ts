dc04ea16dd1a5e18ede0462558070ca4d98154e0
Andrew <andreyka.zaytsev.2017@mail.ru>
2025-06-04 00:31:57 +0300

    final

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: `${process.env.CORS_ORIGIN}`,
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
